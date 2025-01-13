a01c6240b7064f5e4cb5179190fe3339cfb0c51f
CSA Laptop <dawsono@wlhs.wlwv.k12.or.us>
2025-01-13 14:54:41 -0800

    fixed :DDDD

--- src/accountInterface.java
+++ src/accountInterface.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class accountInterface {
     public static void main(String[] args) {
         accountMain main = new accountMain();
-        main.init(5);
+        main.init();
+        Scanner scan = new Scanner(System.in);
+
+        while(true){
+            System.out.println("\nSelect action: \n1 - View Account Information\n2 - Deposit\n3 - Withdraw\n4 - Change Owner\n5 - Interest Rate");
+            int choice = scan.nextInt();
+            switch(choice){
+                case 1:
+                    main.accountInfo();
+                    break;
+                case 2:
+                    System.out.println("How much to deposit?");
+                    main.deposit(scan.nextInt());
+                    scan.nextLine();
+                    main.getBalance();
+                    break;
+                case 3:
+                    System.out.println("How much to withdraw?");
+                    main.withdraw(scan.nextInt());
+                    scan.nextLine();
+                    main.getBalance();
+                    break;
+                case 4:
+                    main.changeowner();
+                    break;
+                case 5:
+                    System.out.print("Interest Rate: ");
+                    double interest = scan.nextDouble();
+                    main.interest(interest);
+                    break;
+            }
+        }
     }
 }
--- src/accountMain.java
+++ src/accountMain.java
@@ -1,30 +1,38 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class accountMain {
     Random rand = new Random();
+    Scanner scan = new Scanner(System.in);
     private double balance;
     private String id;
     private String owner;
-
-
-    public void init(int accountBalance) {
+    public void init() {
+        System.out.print("First and Last Name: ");
+        owner = scan.nextLine().toUpperCase();
+        System.out.print("Initial Account Balance: ");
+        balance = scan.nextDouble();
         id = idConstruct();
-        System.out.println(id);
+        System.out.print("ID: " + id);
+
+    }
+    public void getBalance(){
+        System.out.println("Balance: "+balance);
     }
 
     public String idConstruct() {
-        int id = 0;
-        String idBuild = "";
-        String idLast = "";
-        int[] idArray = new int[8];
+
+        String idArray = "";
         for (int i = 0; i < 8; i++) {
-            int addingnum = rand.nextInt(0, 10);
-            idArray[i] = addingnum;
+            idArray += rand.nextInt(0, 10);
         }
-        for(int i = 0;i<8;i++){
-            idBuild = idLast+""+idArray[i];
-        }
-        return idBuild;
+        return idArray;
+    }
+
+    public void accountInfo(){
+        System.out.println("OWNER: "+owner);
+        System.out.println("ID: "+id);
+        System.out.println("BALANCE: "+balance);
     }
 
     public void deposit(double amount) {
@@ -34,15 +42,22 @@ public class accountMain {
     public void withdraw(double amount) {
         balance -= amount;
     }
-   /* public void changeowner( ) {
 
-
+    public void changeowner() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter account ID to change owner");
-        int userinputid = input.nextInt();
-        if (userinputid.equals(id)) {
-    */
-
+        Integer userinputid = input.nextInt();
+        Integer idasint = Integer.parseInt(id);
+        input.nextLine();
+        if (userinputid.equals(idasint)) {
+            System.out.println("What is the name of the new owner");
+            String newowner = input.nextLine();
+            owner = newowner.toUpperCase();
+            System.out.println(newowner.toUpperCase() + " is the new owner of this account");
+        }
     }
-}
+    public void interest(double interestRate){
+        balance += (balance*interestRate*.1);
+        getBalance();
+    }
 }
